5e9c57c19eaf76959ea7cafc746caecdec075cdd
LukePadawalker <padurariu.luca@gmail.com>
2024-01-15 14:31:39 +0100

    object "team" created

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log ('JS OK');
+
+/*  
+LOGICA
+
+- creiamo l'array team
+- stampiamo in console le info di nome, rolo e la stringa della foto
+- stampare nel DOM le stesse informazioni
+
+
+BONUS
+- trasformare la stringa foto in un'immagine
+- organizzare i membri in card
+
+
+INFO
+Wayne Barnett |	Founder & CEO        |	wayne-barnett-founder-ceo.jpg
+Angela Caroll |	Chief Editor         |	angela-caroll-chief-editor.jpg
+Walter Gordon |	Office Manager       |	walter-gordon-office-manager.jpg
+Angela Lopez  |	Social Media Manager |	angela-lopez-social-media-manager.jpg
+Scott Estrada |	Developer            |	scott-estrada-developer.jpg
+Barbara Ramos |	Graphic Designer     |	barbara-ramos-graphic-designer.jpg
+*/
+
+
+
+
+// array
+const team = {
+    [Name: 'Wayne Barnett', role: 'Founder & CEO', img: 'wayne-barnett-founder-ceo.jpg']
+    [Name: 'Angela Caroll', role: 'Chief Editor', img: 'angela-caroll-chief-editor.jpg']
+    [Name: 'Walter Gordon', role: 'Office Manager', img: 'walter-gordon-office-manager.jpg']
+    [Name: 'Angela Lopez', role: 'Social Media Manager', img: 'angela-lopez-social-media-manager.jpg']
+    [Name: 'Scott Estrada', role: 'Developer', img: 'scott-estrada-developer.jpg']
+    [Name: 'Barbara Ramos', role: 'Graphic Designer', img: 'barbara-ramos-graphic-designer.jpg']
+}
+
+
+
